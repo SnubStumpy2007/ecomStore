5718a45f81aab9077665a1ce2c79becb1d6aaa3a
SnubStumpy2007 <joshua192007@gmail.com>
2023-11-28 18:10:22 -0500

    update

---
+++ src/context/shop-context.jsx
@@ -0,0 +1,20 @@
+import React, { createContext, useState } from "react";
+import { PRODUCTS } from "../products";
+
+
+export const ShopContext = createContext(null)
+
+function getDefaultCart(){
+    let cart = {}
+    for(let i = 1; i < PRODUCTS.length; i++) {
+        cart[i] = 0;
+    }
+    return cart;
+}
+
+export const ShopContextProvider = (props) => {
+    const [cartItems, setCartItems] = useState()
+    return (
+        <ShopContext.Provider>{props.children}</ShopContext.Provider>
+    )
+}
