c82b6338bc3c2935bcc86f3caba41233f401007a
SnubStumpy2007 <joshua192007@gmail.com>
2023-11-30 13:31:08 -0500

    update

--- src/pages/cart/cart-item.jsx
+++ src/pages/cart/cart-item.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 
 function CartItem(props){
-    const {id, productName, price, productImage = props.data}
+    const {id, productName, price, productImage} = props.data
 
     return (
         <div className="cartItem">
         <img src={productImage}/>
         <div className="description">
-
+            <p><b>{productName}</b></p>
+            <p>${price}</p>
         </div>
     </div>
     )
 
 }
 
-export default CartItem
+export default CartItem;
--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,6 +1,8 @@
 import React, { useContext } from "react";
 import { PRODUCTS } from "../../products";
 import { ShopContext } from "../../context/shop-context";
+import CartItem from './cart-item'
+import './cart.css'
 
 function Cart() {
     const { cartItems } = useContext(ShopContext)
@@ -13,7 +15,7 @@ function Cart() {
             <div className="cartItems">
                 {PRODUCTS.map((product) => {
                     if(cartItems[product.id] !== 0) {
-                        return <cartItems data={product}/>
+                        return <CartItem data={product}/>
                     }
                 })}
             </div>
